30798989941cc3b745a5d3b6ce53458b74d415c6
vishnu <vishnu.damuluri@gmail.com>
2024-03-19 10:12:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Navbar = () => {
+  const dataStealingTechniques = [
+    "Phishing",
+    "Malware",
+    // Add more data stealing techniques here...
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <video autoPlay muted loop className="bg-video">
+          <source src="opacity\pexels-florian-lovis-5749164 (2160p)4.mp4" type="video/mp4" />
+          {/* Add additional video sources for compatibility */}
+          Your browser does not support the video tag.
+        </video>
+        <a className="navbar-brand" href="#">Opacity</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            {dataStealingTechniques.map((technique, index) => (
+              <li className="nav-item" key={index}>
+                <a className="nav-link" href="#">{technique}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
-}
+};
 
-export default App;
+export default Navbar;
